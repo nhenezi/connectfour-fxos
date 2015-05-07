236824b586302701516532dec3e31359a6587d93
Nikola Henezi <nikola@henezi.com>
2015-05-07 12:14:57 +0200

    User authentications status loaded during intialization

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -2,11 +2,40 @@
 
 import React from 'react';
 import Router from 'react-router';
+import actions from '../actions.js';
 
 import Header from './Header.jsx';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {}
+    };
+    this.onAuth = this.onAuth.bind(this);
+    this.unsubscribers = [];
+  }
+
+  onAuth(resp) {
+    console.debug('App:onAuth', resp);
+  }
+
+  componentDidMount() {
+    console.debug('App:componentDidMount');
+
+    this.unsubscribers = [
+      actions.auth.completed.listen(this.onAuth)
+    ];
+
+    actions.auth();
+  }
+
+  componentWillUnmount() {
+    this.unsubscribers.map(unsubscribe => unsubscribe());
+  }
+
   render() {
     console.debug('Rendering App');
     return (
