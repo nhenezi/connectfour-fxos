524e1166da887548b14c2afb37558a4f4660a32a
Nikola Henezi <nikola@henezi.com>
2015-07-13 11:23:20 +0200

    Changing header if user is logged in

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,10 +1,56 @@
 'use strict';
 
 import React from 'react';
+import actions from '../actions.js';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false
+    };
+
+    this.onAuth = this.onAuth.bind(this);
+  }
+
+  componentDidMount() {
+    console.debug('Header:componentDidMount');
+
+    this.unsubscribers = [
+      actions.auth.completed.listen(this.onAuth)
+    ];
+  }
+
+  onAuth(resp) {
+    console.debug('Header:onAuth', resp);
+    if (resp.success === true) {
+      this.setState({
+        loggedIn: true
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    console.debug('Header:componentWillUnmount');
+    this.unsubscribers.map(u => u());
+  }
+
   render() {
     console.debug('Header:render');
+
+    let menu = this.state.loggedIn ? (
+      <ul className="left">
+        <li><a href="/#Dashboard">Dashboard</a></li>
+        <li><a href="/#/Logout">Logout</a></li>
+      </ul>
+    ) : (
+      <ul className="left">
+        <li><a href="#/Login">Login</a></li>
+        <li><a href="#/Register">Register</a></li>
+      </ul>
+    );
+
     return (
       <nav className="top-bar" data-topbar="" role="navigation">
         <ul className="title-area">
@@ -15,10 +61,7 @@ class Header extends React.Component {
         </ul>
 
         <section className="top-bar-section">
-          <ul className="left">
-            <li><a href="#/Login">Login</a></li>
-            <li><a href="#/Register">Register</a></li>
-          </ul>
+          {menu}
         </section>
       </nav>
     );
